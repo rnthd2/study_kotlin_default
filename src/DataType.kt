067b8a438fa4fbd1bd24232ec865e23ad5aeceb1
rnthd2 <rnthd2@naver.com>
2021-08-22 15:43:14 +0900

    Kotlin 강좌 #2 - 변수와 자료형

---
+++ src/DataType.kt
@@ -0,0 +1,29 @@
+fun main() {
+//    var a: Int = 123
+//    println(a)
+
+    //nullable variable
+    //npe 발생 할 수 있음
+    var a: Int? = null
+
+    var intValue: Int = 1234
+    var longValue: Long = 1234L
+    var intValueByHex: Int = 0x1234
+    var intValueByBin: Int = 0b10
+
+    var doubleValue: Double = 135.5
+    var doubleValueWithExp: Double = 123.5e10
+    var floatValue: Float = 123.5f
+
+    var charValue: Char = 'a'
+
+    var booleanTrueValue: Boolean = true
+    var booleanFalseValue: Boolean = false
+
+    var stringValue = "one line string test"
+
+    var multiLineStringValue = """multiline 
+ string
+ test"""
+
+}
