c45cfad2ae01ddaafd0b83e967e9addb86e6816c
rnthd2 <rnthd2@naver.com>
2021-08-22 15:44:39 +0900

    Kotlin 강좌 #4 - 타입추론과 함수

---
+++ src/Function.kt
@@ -0,0 +1,11 @@
+/*
+fun add(a: Int, b: Int, c: Int): Int{
+    return a + b + c
+}
+*/
+
+/**
+ * 단일 표현식 함수
+ * 반환형의 타입 추론이 가능하여 타입형 생략 가능
+ */
+fun add(a: Int, b: Int, c: Int) = a + b + c
---
+++ src/TypeInference.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+    var intValue = 123
+    var longValue = 123L
+
+    var doubleValue = 123.5
+    var floatValue = 123f
+
+    var characterValue = '1'
+    var stringValue = "123"
+}
