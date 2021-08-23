b9a96d5e9989dc014e5cf260adeaa8099a98840d
rnthd2 <rnthd2@naver.com>
2021-08-23 14:59:53 +0900

    Kotlin 강좌 #6 - 반복문과 증감연산자

---
+++ src/For.kt
@@ -0,0 +1,11 @@
+fun main(){
+    for(i in 0..9 step 3){
+        print(i)
+    }
+
+    println()
+
+    for(i in 9 downTo 0 step 3){
+        print(i)
+    }
+}
---
+++ src/While.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a = 0
+
+    while(a < 5){
+        println(++a)
+    }
+
+    a = 0
+
+    do {
+        println(++a)
+    } while(a < 5)
+}
